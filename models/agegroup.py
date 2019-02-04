90925ad26e706aeccf967771a3396fc57e951f26
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-04 13:04:30 -0500

    added a name field in age_group

--- models/agegroup.py
+++ models/agegroup.py
@@ -1,8 +1,12 @@
 class AgeGroup:
-    def __init__(self, min_age, max_age):
+    def __init__(self, name, min_age, max_age):
+        self._name = name
         self._min_age = min_age
         self._max_age = max_age
     
+    def getName(self):
+        return self._name
+        
     def getMinAge(self):
         return self._min_age
     
