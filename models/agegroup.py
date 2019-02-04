715ddf400cc10d1f3760fe5a60d8e31b99040e4c
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-04 10:07:02 -0500

    made a simple pojo for age_group

---
+++ models/agegroup.py
@@ -0,0 +1,14 @@
+class AgeGroup:
+    def __init__(self, min_age, max_age):
+        self._min_age = min_age
+        self._max_age = max_age
+    
+    def getMinAge(self):
+        return self._min_age
+    
+    def getMaxAge(self):
+        return self._max_age
+
+    def verifySwimmer(self, swimmer):
+        raise NotImplemented('yet :)')
+    
