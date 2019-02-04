f18a67cb24c5413f34caf97325d070f628bc751a
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-04 13:04:53 -0500

    added logic to age_group_service

--- serivices/age_group_service.py
+++ serivices/age_group_service.py
@@ -4,7 +4,36 @@ from models import *
 
 class AgeGroupService:
     def __init__(self):
-        pass
-    
-    def getByName(group_name):
-        raise Exception('could noy find an age group by that name')
+        # assume this is a database
+        self._data_store = [
+            {
+                'name': 'teenager',
+                'min_age' : 13,
+                'max_age' : 18
+            },
+            {
+                'name': 'toddler',
+                'min_age' : 1,
+                'max_age' : 5
+            },
+            {
+                'name': 'youth',
+                'min_age' : 18,
+                'max_age' : 25
+            }
+        ]
+
+    def getByName(self, group_name):
+        data_entry = filter(lambda entry: entry['name'] == group_name, self._data_store)
+        
+        try:
+            data_entry = data_entry[0]
+            if not data_entry: raise
+        except:
+            raise Exception('could not find an age group by that name')
+
+        return AgeGroup(
+            data_entry['name'],
+            data_entry['min_age'],
+            data_entry['max_age']
+        )
