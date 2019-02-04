57e6f95f629054c8c53cffd47b87ea872ea82758
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-04 12:43:59 -0500

    created a simple service for age_group model

---
+++ serivices/age_group_service.py
@@ -0,0 +1,10 @@
+import sys, os.path; sys.path.append(os.path.abspath('..'))
+
+from models import *
+
+class AgeGroupService:
+    def __init__(self):
+        pass
+    
+    def getByName(group_name):
+        raise Exception('could noy find an age group by that name')
