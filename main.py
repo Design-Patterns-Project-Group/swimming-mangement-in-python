d674e8789dcd913fe0efd24d32d77c0a6a545930
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-06 07:05:16 -0500

    fixed typo 'serivices' -> 'services' on the module name

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+#!/usr/bin/python3
+from models import *
+from serivices import *
+
 def main():
-    pass
+    # season1 = Season('2018 summer', '1/1/2018')
+    # season2 = Season('2018 summer', '1/1/2018')
+    
+    # age_group_service = AgeGroupService()
+    # swimmer_service = SwimmerService()
+    season_service = SeasonService()
+    score_service = ScoreService()
+
+    for season in season_service.getAll():
+        print(season.getName())
+
+    for score in score_service.getAllBySeasonName('ethiopia_season1_2018'):
+        print(score.getSwimmer().getFullname())
 
 if __name__ == '__main__':
     main()
