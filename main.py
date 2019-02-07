5c1fca579d0d56d6f287ca7f5e58c11d5f6fe894
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-06 19:46:47 -0500

    refactored main_view to accept whole lotta services @ once via injection

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
 from models import *
-from services import *
+import services
 from views.main_view import MainView
 
 def main():
-    season_service = SeasonService()
-    score_service = ScoreService()
-
-    MainView(score_service, season_service)
+    MainView(services)
 
 if __name__ == '__main__':
     main()
--- views/main_view.py
+++ views/main_view.py
@@ -4,9 +4,9 @@ from tkinter import *
 # TODO : implement the Tkinter GUI
 
 class MainView(AbstractMainView):
-    def __init__(self, score_service, season_service):
-        self._score_service = score_service
-        self._season_service = season_service
+    def __init__(self, services):
+        self._score_service = services.ScoreService()
+        self._season_service = services.SeasonService()
         self.prepareGUI()
     
     def prepareGUI(self):
