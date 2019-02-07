6c33e125afadc765978a643c606c8f3770ba7b0a
Tibebeselasie Mehari <tibebes.js@gmail.com>
2019-02-06 19:35:41 -0500

    added the services into the MainView via dependency injection

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 #!/usr/bin/python3
 from models import *
 from services import *
+from views.main_view import MainView
 
 def main():
-    # season1 = Season('2018 summer', '1/1/2018')
-    # season2 = Season('2018 summer', '1/1/2018')
-    
-    # age_group_service = AgeGroupService()
-    # swimmer_service = SwimmerService()
     season_service = SeasonService()
     score_service = ScoreService()
 
-    for season in season_service.getAll():
-        print(season.getName())
-
-    for score in score_service.getAllBySeasonName('ethiopia_season1_2018'):
-        print(score.getSwimmer().getFullname())
+    MainView(score_service, season_service)
 
 if __name__ == '__main__':
     main()
